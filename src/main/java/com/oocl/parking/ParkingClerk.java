bd22b9460457b61923000ff74ebd7839927aa40a
Jason Liu <jason.liu@oocl.com>
2018-12-07 09:56:14 +0800

    [J]: chore: set up main function

--- src/main/java/com/oocl/parking/ParkingClerk.java
+++ src/main/java/com/oocl/parking/ParkingClerk.java
@@ -1,6 +1,11 @@
 package com.oocl.parking;
 
 public class ParkingClerk {
+        public static void main(String[] args) {
+            System.out.println("Hello! World!");
+        }
+
+
     public boolean test(){
         return true;
     }
