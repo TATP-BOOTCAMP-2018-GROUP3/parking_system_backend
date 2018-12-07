1fd146eed730b8db6d83fba8c707e939a7beed7b
Jason Liu <jason.liu@oocl.com>
2018-12-07 16:38:29 +0800

    [J]: refactor rename table column

--- src/main/resources/db/migration/V2__create_order_table.sql
+++ src/main/resources/db/migration/V2__create_order_table.sql
@@ -1,7 +1,7 @@
 create table parking_order(
     id bigint not null auto_increment,
     car_Id varchar(64) not null,
-    parking_lot_id varchar(64),
+    parking_lot varchar(64),
     status varchar(64),
     primary key (id)
 )
