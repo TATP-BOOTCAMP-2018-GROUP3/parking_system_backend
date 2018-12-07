6f25c39c1bab004b3b890ce86f1a16f4ff0b2d62
Jason Liu <jason.liu@oocl.com>
2018-12-07 14:16:22 +0800

    [J]: chore: set up basic class for backend

--- src/main/java/com/oocl/parking/domain/ParkingClerk.java
+++ src/main/java/com/oocl/parking/domain/ParkingClerk.java
@@ -1,13 +1,31 @@
 package com.oocl.parking.domain;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "parking_clerk")
 public class ParkingClerk {
 
-    public boolean test(){
-        return true;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "account_name", unique = true)
+    private String accountName;
+
+    protected ParkingClerk(){}
+
+    public ParkingClerk(String name){
+        this.accountName = name;
     }
 
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
 }
