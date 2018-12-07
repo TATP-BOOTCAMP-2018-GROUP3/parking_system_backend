d21164316bd91a43787802272ddd302d22cbbb63
Antony Chan <antony.chan@oocl.com>
2018-12-07 10:41:21 +0800

    [Antony] update gradle dependency

--- src/main/java/com/oocl/parking/ParkingSystemApplication.java
+++ src/main/java/com/oocl/parking/ParkingSystemApplication.java
@@ -1,12 +1,13 @@
 package com.oocl.parking;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ParkingSystemApplication {
-    /*
+    
     public static void main(String [] args){
-        SpringBootApplication.run(ParkingSystemApplication.class,args);
+        SpringApplication.run(ParkingSystemApplication.class,args);
     }
-    */
+    
 }
