383dac3563e019863c1c48482a9dee4253e36efd
Jason Liu <jason.liu@oocl.com>
2018-12-07 16:06:12 +0800

    [J]:test : add test for POST method

--- src/test/java/com/oocl/parking/ParkingClerkTest.java
+++ src/test/java/com/oocl/parking/ParkingClerkTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,6 +19,7 @@ import static com.oocl.parking.WebTestUtil.getContentAsObject;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @RunWith(SpringRunner.class)
@@ -37,7 +39,7 @@ public class ParkingClerkTest {
     }
 
     @Test
-    public void get_parking_boy_test() throws Exception
+    public void get_parking_clerk_test() throws Exception
     {
         //g
         ParkingClerk clerk = new ParkingClerk("Test1");
@@ -48,5 +50,20 @@ public class ParkingClerkTest {
         assertEquals(200, result.getResponse().getStatus());
         final ParkingClerkResponse[] responses = getContentAsObject(result, ParkingClerkResponse[].class);
         assertEquals("Test1", responses[0].getAccountName());
+    }
+
+    @Test
+    public void post_parking_clerk_test() throws Exception
+    {
+        //g
+        String clerkJson = "{\"accountName\":\"Test2\"}";
+        //w
+        final MvcResult result = mvc.perform(post("/parkingclerks")
+        .contentType(MediaType.APPLICATION_JSON).content(clerkJson)).andReturn();
+
+        //t
+        assertEquals(201, result.getResponse().getStatus());
+        assertEquals("Test2", parkingClerkRepository.findAll().get(0).getAccountName());
+
     }
 }
