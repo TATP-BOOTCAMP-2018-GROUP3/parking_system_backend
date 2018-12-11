a58b4724a54d6f031cf1e1c77851cc071e043387
Jason Liu <jason.liu@oocl.com>
2018-12-11 17:06:55 +0800

    [J] feat: add employee getbyId

--- src/main/java/com/oocl/parking/controllers/EmployeeResource.java
+++ src/main/java/com/oocl/parking/controllers/EmployeeResource.java
@@ -1,14 +1,14 @@
 package com.oocl.parking.controllers;
 
 
+import com.oocl.parking.domain.Employee;
 import com.oocl.parking.models.EmployeeResponse;
 import com.oocl.parking.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -27,6 +27,15 @@ public class EmployeeResource {
                 .toArray(EmployeeResponse[]::new);
 
         return ResponseEntity.ok(employees);
+    }
+    @GetMapping(value = "{/id}")
+    public ResponseEntity<EmployeeResponse> getById(@PathVariable Long id)
+    {
+        final Optional<Employee> e = employeeRepository.findById(id);
+        if (!e.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(EmployeeResponse.create(e.get()));
     }
 
 
