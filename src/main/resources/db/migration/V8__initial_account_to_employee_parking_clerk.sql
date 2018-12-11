126a1b7edf2ec516e70e18c278d70e58fee80cd8
Antony Chan <antony.chan@oocl.com>
2018-12-12 04:26:48 +0800

    [Antony] feat: add initial account

---
+++ src/main/resources/db/migration/V8__initial_account_to_employee_parking_clerk.sql
@@ -0,0 +1,3 @@
+INSERT INTO EMPLOYEE VALUES(default, 'CHAN Tai Man', 'clerk@email.com', '29187232', 'CLERK', 'clerk', 'e0886e974213e36aff4aa0a8dde2e183e6f89cb30079f1de60b7034bea776334', 'RKQVu05u', 'On Duty');
+
+INSERT INTO PARKING_CLERK VALUES(default, 1, 'Available');
