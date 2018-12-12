7eafe523faf19a594b1bd9d0c6acea7d03b93919
Antony Chan <antony.chan@oocl.com>
2018-12-12 11:35:07 +0800

    [Antony] feat: add initial account for manager & admin

---
+++ src/main/resources/db/migration/V9__initial_account_for_admin_manager.sql
@@ -0,0 +1,2 @@
+INSERT INTO EMPLOYEE VALUES(default, 'CHAN Siu Man', 'admin@email.com', '29187121', 'ADMIN', 'admin', 'a7dccd0d6c568a09743932f9fb03d8115defd1ae60f4c90e0b228748afda3f79', 'IZSzG10X', 'On Duty');
+INSERT INTO EMPLOYEE VALUES(default, 'CHAN Dou Chai', 'manager@email.com', '291872222', 'MANAGER', 'manager', '1b979af5e59813e2892e8eb1236a6b57439dfef50187c07f43dca29c2ad13df0', 'ECWJtPmU', 'On Duty');
